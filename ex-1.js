42c000c7269be1e1a5c1655fc45a14c89f9e3907
Pond <pondwachi.th@gmail.com>
2024-05-09 14:55:26 +0700

    feat: JS 0f pond

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,9 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
+isOver18 = 18;
+hasCriminalBlacklist = false;
 
-let isAllow;
+let isAllow = (isOver18 && !hasCriminalBlacklist);
 
-// Start coding here
 
 console.log(isAllow);
