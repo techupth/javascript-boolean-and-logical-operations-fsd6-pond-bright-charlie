af13538bb9e3556c7f1aa6b891e2052fe85f2b47
Sunbright53 <141810435+Sunbright53@users.noreply.github.com>
2024-05-09 16:24:19 +0700

    fix:bug

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,6 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
 
 // Start coding here
-
+let isAllow = isOver18 && !hasCriminalBlacklist;
 console.log(isAllow);
